ffda4867ae767768f090ff8dfdc861ea327a4e95
velbaumr <58743184+velbaumr@users.noreply.github.com>
2024-09-24 14:39:45 +0300

    Code cleanup

--- Tasks/Controllers/TaskController.cs
+++ Tasks/Controllers/TaskController.cs
@@ -74,10 +74,4 @@ public class TaskController(ITasksService tasksService) : Controller
         tasksService.RemoveTask(task);
         return RedirectToAction(nameof(Index));
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
